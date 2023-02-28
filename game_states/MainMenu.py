ac23afc13c659f9469c8d0c6fa51669dccdb5abb
Luke Confait <luke.confait@gmail.com>
2023-02-28 21:38:12 +0200

    Features: added starte and quit button functionality

--- game_states/MainMenu.py
+++ game_states/MainMenu.py
@@ -1,8 +1,9 @@
 import pygame
 
 from pygame.locals import (
-    K_ESCAPE,
     K_RETURN,
+    K_UP,
+    K_DOWN
 )
 
 from game_states.State import State
@@ -13,7 +14,6 @@ class MainMenu(State):
     """
     def __init__(self):
         super().__init__()
-        self.next = 'game'
         self.name = 'main_menu'
         self.fps = 15
     
@@ -27,6 +27,7 @@ class MainMenu(State):
         """ 
         Performs initialisation for the MainMenu
         """
+        self.buttons_index = 0 
         print('starting up menu state')
         
     def get_event(self, event) -> None:
@@ -34,10 +35,20 @@ class MainMenu(State):
         Event listener
         """
         if event.type == pygame.KEYDOWN:
-            if event.key == K_ESCAPE:
-                print('escape pressed')
+
+            if event.key == K_DOWN and self.buttons_index == 0:
+                self.buttons_index = 1
+            elif event.key == K_UP and self.buttons_index == 1:
+                self.buttons_index = 0
+
             if event.key == K_RETURN:
-                self.done = True
+                if self.buttons_index == 0:
+                    self.next = 'game'
+                    self.done = True
+                if self.buttons_index == 1:
+                    self.done = True
+                    self.quit = True
+
 
     def update(self, screen) -> None:
         """
@@ -55,17 +66,36 @@ class MainMenu(State):
         title_font = pygame.font.SysFont("Verdana", size=32)
         small_font = pygame.font.SysFont("Verdana", size=20)
 
+        inactive_text_color = (255, 255, 255)
+        active_text_color = (255, 0, 0)
+
         title_box = pygame.Surface((400, 200))
         title_text = title_font.render(
             "Untitled bullet hell game", True, (255, 255, 255), (0, 0, 0)
         )
         title_rect = title_text.get_rect(center=(200, 100))
 
+
         start_box = pygame.Surface((200, 100))
-        start_text = small_font.render("Start", True, (255, 255, 255), (0, 0, 0))
+        color = inactive_text_color
+        if self.buttons_index == 0:
+            color = active_text_color
+        start_text = small_font.render("Start", True, color, (0, 0, 0))
         start_rect = start_text.get_rect(center=(100, 50))
+
+
+        quit_box = pygame.Surface((200,100))
+        color = inactive_text_color
+        if self.buttons_index == 1:
+            color = active_text_color
+        quit_text = small_font.render("Quit", True, color, (0, 0, 0))
+        quit_rect = quit_text.get_rect(center=(100,50))
 
         title_box.blit(title_text, title_rect)
         screen.blit(title_box, (440, 100))
+
         start_box.blit(start_text, start_rect)
         screen.blit(start_box, (540, 400))
+
+        quit_box.blit(quit_text,quit_rect)
+        screen.blit(quit_box, (540, 600))
