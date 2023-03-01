ed3f19696deb274780d155e861fd215b9463f67a
Luke Confait <luke.confait@gmail.com>
2023-03-01 18:02:18 +0200

    Improved scoreboard and added game over screen

--- game_states/Game.py
+++ game_states/Game.py
@@ -35,7 +35,7 @@ class Game(State):
         """
         print("starting up game state")
         # set a font for use in displaying score and other metrics
-        self.font = pygame.font.SysFont("Verdana", size=20)
+        self.font = pygame.font.SysFont("Verdana", size=40)
         # set up play area
         self.play_area = pygame.Surface((config.GAME_WIDTH, config.GAME_HEIGHT))
         self.play_area.fill((255,255,255))
@@ -49,6 +49,8 @@ class Game(State):
         self.bullets = pygame.sprite.Group()
         # setup game score
         self.score =  0
+        
+        self.game_over = False
 
     def get_event(self, event) -> None:
         """
@@ -58,31 +60,35 @@ class Game(State):
             if event.key == K_ESCAPE:
                 print('escape pressed')
                 paused()
-            if event.key == K_RETURN:
+
+            if event.key == K_RETURN and self.game_over == True:
                 self.done = True
-        if event.type == self.ADDBULLET:
-            new_bullet = Bullet()
-            self.bullets.add(new_bullet)
-            
-            self.all_sprites.add(new_bullet)
-        
-        
-        pressed_keys = pygame.key.get_pressed()
-        self.player.update(pressed_keys)
+
+        if self.game_over == False:
+
+            if event.type == self.ADDBULLET:
+                new_bullet = Bullet()
+                self.bullets.add(new_bullet)
+                self.all_sprites.add(new_bullet)
+
+            pressed_keys = pygame.key.get_pressed()
+            self.player.update(pressed_keys)
 
     def update(self, screen) -> None:
         """
         Non event loop game logic
         """
+        if not self.game_over:
+            self.bullets.update()
 
-        self.bullets.update()
-        for bullet in self.bullets:
-            if bullet.kill_next_frame == True:
-                self.score += 15
+            # This feels kinda bad but im unsure how to implement it into the bullet update
+            for bullet in self.bullets:
+                if bullet.kill_next_frame == True:
+                    self.score += 15
         
         if pygame.sprite.spritecollideany(self.player, self.bullets):
-            self.player.kill()
-            self.done = True
+            self.game_over = True
+
         self.draw(screen)
 
     def draw(self, screen) -> None:
@@ -92,16 +98,34 @@ class Game(State):
         screen.fill((0,0,0))
         screen.blit(self.play_area, (30, 30))
 
+        score_title = pygame.Surface((390,100))
+        score_title.fill((255,255,255))
+        score_title_text = self.font.render("Score", True, (0,0,0))
+        score_title_rect = score_title_text.get_rect(center=(195,50))
+
+        score_title.blit(score_title_text, score_title_rect)
+        screen.blit(score_title,(860, 30))
+
         score_board = pygame.Surface((390,100))
         score_board.fill((255,255,255))
-        score_text = self.font.render(f"{self.score}", True, (0,0,0))
-        score_rect = score_text.get_rect(center=(195,25))
+        score_board_text = self.font.render(f"{self.score}", True, (0,0,0))
+        score_rect = score_board_text.get_rect(right=390,centery=50)
 
-        score_board.blit(score_text, score_rect)
+        score_board.blit(score_board_text, score_rect)
         screen.blit(score_board,(860, 140))
 
         for entity in self.all_sprites:
             screen.blit(entity.surf, entity.rect)
+
+        if self.game_over == True:
+            game_over_block = pygame.Surface((400,100))
+            game_over_block.fill((255,255,255))
+            # game_over_text = self.font.render(f"Haha you suck",True,(0,0,0))
+            game_over_text = self.font.render(f"GAME OVER",True,(0,0,0))
+            game_over_rect = game_over_text.get_rect(center=(200,50))
+
+            game_over_block.blit(game_over_text,game_over_rect)
+            screen.blit(game_over_block,(230, 130))
 
 def paused() -> None :
     """
